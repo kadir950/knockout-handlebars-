385d9dd0a0120daf617f6837b5147923b939fc80
kadir950 <kadir950@hotmail.com>
2023-04-09 22:09:14 +0300

    Create ko-handlebars.js

---
+++ ko-handlebars.js
@@ -0,0 +1,50 @@
+
+    //Test template >>>  `<div class="deneme"><b data-bind="text: $data.name"></b> {{$data.name}}</div>`
+
+    Handlebars.registerHelper("_koEvaluator", function (_val, _args) {
+        if (!_val) return;       
+        return new Handlebars.SafeString(ko.bindingProvider.instance.parseBindingsString("myExp:" + _val, _args.data.root.koBindingContext)["myExp"]);
+    });
+
+
+    (function () {
+
+        const HandlebarsEngine = function () {
+
+            function executeTemplate(compiledTemplate,  handlebarsTemplateOptions) {
+                return  compiledTemplate(handlebarsTemplateOptions, {
+                    allowProtoMethodsByDefault: true,
+                    allowProtoPropertiesByDefault: true
+                });
+            }
+
+            this.renderTemplateSource = function (templateSource, bindingContext, _options, _templateDocument) {
+               
+                let precompiled = templateSource['data']('precompiled');
+
+                if (!precompiled) {
+                    const templateText = `{{#with koBindingContext}}${(templateSource['text']() ?? "")}{{/with}}`;
+                    precompiled = Handlebars.compile(templateText, {});
+                    templateSource['data']('precompiled', precompiled);
+                }
+                                
+                const resultNodes = executeTemplate(precompiled, { 'koBindingContext': bindingContext });
+
+
+                const divx = document.createElement('div');
+                debugger;
+                divx.innerHTML = resultNodes;
+                return Array.from(divx.children);
+            };
+
+            this.createJavaScriptEvaluatorBlock = function (script) {
+                return `{{_koEvaluator "${script}" }}`;
+            };        
+        };
+
+        HandlebarsEngine.prototype = new ko.templateEngine();
+        HandlebarsEngine.prototype.constructor = HandlebarsEngine;
+
+        ko.HandlebarsEngine = HandlebarsEngine;
+        ko.setTemplateEngine(new HandlebarsEngine());
+    })();
